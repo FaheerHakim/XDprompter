7394ff312182d4c26edf384a9fc2d531e1a9b276
FaheerHakim <r0820158@student.thomasmore.be>
2023-05-08 23:31:53 +0200

    added signup form to index.php

--- home.php
+++ home.php
@@ -5,17 +5,20 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">  
 	
-	<title>LogIn</title>
+	<title>Sign Up</title>
 	<link rel="stylesheet" href="css/style.css">
 </head>
 <body>
      <form action="login.php" method="post">
-     	<h2>Login</h2>
+     	<h2>Sign Up</h2>
      	<?php if (isset($_GET['error'])) { ?>
      		<p class="error"><?php echo $_GET['error']; ?></p>
      	<?php } ?>
      	<label>User Name</label>
      	<input type="text" name="username" placeholder="username"><br>
+
+		 <label>E-mail</label>
+     	<input type="text" name="email" placeholder="pppp@outlook.com"><br>
 
      	<label>Password</label>
      	<input type="password" name="password" placeholder="password"><br>
--- login.php
+++ login.php
@@ -15,5 +15,32 @@ if(!empty($_POST)) {
            $error = "Invalid username or password!";
         }
 
-    $user = new Prompter();    
+    //$user = new User();    
 }
+
+?><!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">  
+	
+	<title>Welcome back prompter!</title>
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+     <form action="login.php" method="post">
+     	<h2>Login</h2>
+     	<?php if (isset($_GET['error'])) { ?>
+     		<p class="error"><?php echo $_GET['error']; ?></p>
+     	<?php } ?>
+     	<label>User Name</label>
+     	<input type="text" name="username" placeholder="username"><br>
+
+     	<label>Password</label>
+     	<input type="password" name="password" placeholder="password"><br>
+
+     	<button type="submit">Login</button>
+     </form>
+</body>
+</html>
